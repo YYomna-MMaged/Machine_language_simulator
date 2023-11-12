f90e6cedb523d192943b5f8c1f8428a40593ba3b
Yomna <14.yomn@gmail.com>
2023-11-12 19:46:25 +0200

    first step

---
+++ Simulator.h
@@ -0,0 +1,16 @@
+
+#ifndef OOP_ASSIGNMENT2_TASK3_SIMULATOR_H
+#define OOP_ASSIGNMENT2_TASK3_SIMULATOR_H
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class CPU{
+
+};
+
+class memory{
+
+};
+#endif //OOP_ASSIGNMENT2_TASK3_SIMULATOR_H
---
+++ main.cpp
@@ -0,0 +1,16 @@
+// File: A2_Task3_S19_20221196-20221062-20221041.cpp
+// Purpose:
+// Author: Yomna Maged Ali 20221196
+//         Rawan Amr Nabil 20221062
+//         Tasnem Mamdoh 20221041
+// Section: S19
+// TA: Mohamed Talaat
+#include <iostream>
+#include <bits/stdc++.h>
+#include "Simulator.h"
+using namespace std;
+
+int main() {
+
+    return 0;
+}
