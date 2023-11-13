29fb73e2fdb085d8d9838ff0ed78e43127fccb89
Tasneem31 <tasneem.mansy1@gmail.com>
2023-11-13 22:07:44 +0200

    construction

--- classes.cpp
+++ classes.cpp
@@ -35,7 +35,7 @@ void Machine :: execute() {
     char opcode;
     char operad1;
     string operad2;
-
+    int i;
     for (int i = 0; i < mainmemory->size; i+=2) {
         IR = mainmemory->insrtuctions[i].second + mainmemory->insrtuctions[i+1].second;
 
@@ -43,11 +43,11 @@ void Machine :: execute() {
         operad1 = IR[1];
         operad2 = IR.substr(2, 2);
 
-        instructions(opcode , operad1 , operad2);
+        instructions(opcode , operad1 , operad2,i);
     }
 }
 
-void Machine::instructions(char op, char o1, string o2) {
+void Machine::instructions(char op, char o1, string o2,int i) {
 
     if(op == '1')
     {
@@ -68,19 +68,19 @@ void Machine::instructions(char op, char o1, string o2) {
     }
     else if(op == '4')
     {
-        // code
+        movecontant(o2);
     }
     else if(op == '5')
     {
-        // code
+        add(o1,o2);
     }
     else if(op == 'B')
     {
-        // code
+         jump(o1,o2,i);
     }
     else if(op == 'C')
     {
-        // code
+        Halt();
     }
 
 }
@@ -91,7 +91,6 @@ void Machine :: loadcontant(char o1, string o2) {
 
     int memindex = stoi(o2 , 0 , 16);
     Register[to_string(o1)] = mainmemory->insrtuctions[memindex].second;
-
 }
 //Operation 2---------------
 void Machine :: loadbit(char o1 , string o2) {
@@ -107,3 +106,29 @@ void Machine ::storeincell(char o1 , string o2) {
 void Machine ::store00(char o1) {
 
 }
+//operation 5----------------
+void Machine ::movecontant(string o2){
+   Register [to_string(o2[1])] = Register[to_string(o2[0])];
+}
+//operation 6----------------
+void Machine ::add(char o1 , string o2) {
+     int x =  stoi (Register [to_string(o2[1])]);
+     int y= stoi( Register[to_string(o2[0])]) ;
+    Register[to_string(o1)]=to_string(char (x)+char (y) - char(256));
+}
+
+//Operation B ---------------
+void Machine::jump(char o1, string o2,int i) {
+    if (Register[to_string(o1)] == Register["00"]) {
+        int memindex = stoi(o2, 0, 16);
+        i =memindex;
+    }
+
+}
+
+//Operation C ---------------
+void Machine ::Halt(){
+    return ;
+}
+
+
